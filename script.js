379e83819de2077b7ad413fdf8ab5538276af879
squirrel-sv <v.shindyruk@ukr.net>
2025-07-02 16:10:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide');
+
+slides[0].classList.add('active');
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
